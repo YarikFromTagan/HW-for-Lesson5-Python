79267d8a2318b467c147edf0b01bbd6aebe701f3
NB_Yarik_from_Tagan <y.krivoff@gmail.com>
2022-11-09 01:28:35 +0300

    Edit  in Task3

--- Task3.py
+++ Task3.py
@@ -11,8 +11,6 @@ def draw_board(board):
         print('|', board[0 + i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|')
         print('-------------')
 
-#draw_board(board)
-
 def take_input(player_token):
     valid = False
     while not valid:
